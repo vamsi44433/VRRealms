a488bda5ef207a88ae1382ceac5c1f2365873403
vamsi44433 <indelavamsi444@gmail.com>
2025-07-10 09:26:49 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,14 +42,6 @@
 
 	// Nav.
 
-		// Title Bar.
-			$(
-				'<div id="titleBar">' +
-					'<a href="#navPanel" class="toggle"></a>' +
-					'<span class="title">' + $('#logo').html() + '</span>' +
-				'</div>'
-			)
-				.appendTo($body);
 
 		// Panel.
 			$(
